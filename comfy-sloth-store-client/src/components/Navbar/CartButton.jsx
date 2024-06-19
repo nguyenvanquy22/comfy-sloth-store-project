46372ca559db6cda109b2c659eeaa72028064bf4
nguyenvanquy22 <nguyenvanquy9a2003@gmail.com>
2024-06-19 08:52:21 +0700

    add submenu

--- comfy-sloth-store-client/src/components/Navbar/CartButton.jsx
+++ comfy-sloth-store-client/src/components/Navbar/CartButton.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { FaShoppingCart, FaUserMinus, FaUserPlus } from 'react-icons/fa'
+import { FaShoppingCart, FaUserPlus, FaUser } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 import { useAppContext } from '../../contexts/app_context'
 import { useCartContext } from '../../contexts/cart_context'
@@ -20,9 +20,15 @@ function CartButton() {
         </span>
       </Link>
       {authenticated ? (
-        <Link className='auth-btn' onClick={logout}>
-          {user.username} <FaUserPlus />
-        </Link>
+        <div className='user-options'>
+          <Link className='auth-btn'>
+            {user.username} <FaUser />
+          </Link>
+          <ul className='sub-links'>
+            <li><Link to='/profile' onClick={closeSidebar}>profile</Link></li>
+            <li><Link onClick={logout} >log out</Link></li>
+          </ul>
+        </div>
       ) : (
         <Link to='/login' className='auth-btn'>
           Login <FaUserPlus />
@@ -42,7 +48,6 @@ const Wrapper = styled.div`
     color: var(--clr-grey-1);
     font-size: 1.5rem;
     letter-spacing: var(--spacing);
-    color: var(--clr-grey-1);
     display: flex;
 
     align-items: center;
@@ -83,6 +88,50 @@ const Wrapper = styled.div`
     svg {
       margin-left: 5px;
     }
+  }
+  .user-options {
+    position: relative;
+    padding: 1rem 0;
+  }
+  .sub-links {
+    position: absolute;
+    display: none;
+    background: var(--clr-primary-5);
+    width: 150px;
+    padding: 0.5rem 1.5rem;
+    margin-top: 1rem;
+    z-index: 3;
+    box-shadow: var(--dark-shadow);
+    border-radius: var(--radius);
+    transform: translateX(-15%);
+    transition: var(--transition);
+  }
+  .sub-links a {
+    display: block;
+    font-size: 1.2rem;
+    color: var(--clr-grey-10);  
+    cursor: pointer;
+    padding: 0.2rem;
+    text-transform: capitalize;
+  }
+  .sub-links a:hover {
+    // color: var(--clr-grey-8);  
+  }
+  .user-options:hover .sub-links {
+    display: block;
+  }
+  .sub-links::before {
+    content: '';
+    display: block;
+    width: 0;
+    height: 0;
+    border-left: 5px solid transparent;
+    border-right: 5px solid transparent;
+    border-bottom: 5px solid var(--clr-primary-5);
+    position: absolute;
+    top: -5px;
+    left: 50%;
+    transform: translateX(-50%);
   }
 `
 
